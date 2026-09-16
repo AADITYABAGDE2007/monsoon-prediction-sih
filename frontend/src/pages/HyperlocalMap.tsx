70c729494f199b9df41431f308d3fe97f1fb2aba
AADITYABAGDE2007 <aadityabagde@users.noreply.github.com>
2026-09-17 02:01:43 +0530

    fix: replace CARTO basemaps with official OpenStreetMap tiles to remove API key required watermark

--- frontend/src/pages/HyperlocalMap.tsx
+++ frontend/src/pages/HyperlocalMap.tsx
@@ -133,8 +133,8 @@ export default function HyperlocalMap() {
             style={{ height: '100%', width: '100%' }}
           >
             <TileLayer
-              attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-              url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
             {filteredFeatures.map((f) => (
